b1b30607de7cec7a01fa0da5ef53f114b29fa14b
Brendon Lee <brendonclee@gmail.com>
2025-12-14 09:59:22 +0800

    feat: add placeable towers

--- wave_spawner.gd
+++ wave_spawner.gd
@@ -1,9 +1,11 @@
 extends Node2D
 
 @export var enemy_scene: PackedScene
-@export var enemies_in_wave := 5
-@export var spawn_delay := 0.8
+@export var enemies_in_wave := 25
+@export var spawn_delay := 2.5
 @export var follower_scene: PackedScene
+@export var tower_scene: PackedScene
+@export var placement_enabled := true
 
 @onready var path := $Path2D
 
@@ -23,3 +25,11 @@ func spawn_enemy():
 	var enemy := enemy_scene.instantiate()
 	enemy.position = Vector2.ZERO
 	follower.add_child(enemy)
+
+func _unhandled_input(event):
+	if not placement_enabled:
+		return
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		var tower := tower_scene.instantiate()
+		tower.global_position = get_global_mouse_position()
+		add_child(tower)
